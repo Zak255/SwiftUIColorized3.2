1ff496600260db7bfdff8056dc53f4d6c73b9e62
Zakir.Kazimov <ramzan666666@gmail.com>
2021-09-14 20:29:48 +0300

    Initial Commit

---
+++ SwiftUIColorized3.2/SwiftUIColorized3_2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIColorized3_2App.swift
+//  SwiftUIColorized3.2
+//
+//  Created by Zakir on 14.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIColorized3_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
